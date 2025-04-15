671fb3f3d9fb4ed5794ce7540f2581d4d2df0223
Rohit Khatri <rohitkhatri111112@gmail.com>
2025-04-15 13:15:05 +0530

    React Spline Project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='border h-screen relative'>
+      <Header />
+      <main>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='p-10 border'>
+
+        </header>
+    )
+}
+
+export default Header
